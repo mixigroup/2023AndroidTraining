f0e1f1eb9799f2c91cce6629ccccef0d909356e2
yasuhiro.oida <yasuhiro.oida@mixi.co.jp>
2023-04-17 19:57:30 +0900

    BookItemを実装

--- app/ui/src/main/java/jp/co/mixi/androidtraining/ui/BookItem.kt
+++ app/ui/src/main/java/jp/co/mixi/androidtraining/ui/BookItem.kt
@@ -1,16 +1,47 @@
 package jp.co.mixi.androidtraining.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun BookItem(
     book: Book,
     modifier: Modifier = Modifier
 ) {
-    // TODO 演習 3.1
+    Row(
+        modifier = modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Text(
+                text = book.title,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = book.author,
+                fontSize = 16.sp
+            )
+        }
+        Button(onClick = { }) {
+            Text(text = "読む")
+        }
+    }
 }
 
 @Preview
